6e2369954da6d830c6a1789c371b5d022effc082
racheleldridge <racheleldridge99@gmail.com>
2018-07-03 16:53:06 +0100

    The start of adding dvla add car
    the start of the code for the new add car. this is where it goes to the dvla to get the information

---
+++ _temp/dvla/motcheck.php
@@ -0,0 +1,55 @@
+<?php
+
+define("APIKEY","ptZAjbJWQO2LSzitce0JM9wZo4ASO2yV7XWGmoD7");
+define("ROOTURL","https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests");
+
+$_POST['creg'] = "rf12hxa";
+
+$myreg  = preg_replace('/[^a-zA-Z0-9]/','',$_POST['creg']);
+
+$errors = array();
+$jsonout = array();
+
+if (strlen($myreg)>2) {
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,ROOTURL."?registration=".$myreg);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json","x-api-key:".APIKEY));
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	$response = curl_exec($ch);
+
+	if (curl_errno($ch))   {
+		$error['errors'] = 'ERROR -> ' . curl_errno($ch) . ': ' . curl_error($ch);
+	} else {
+		if (stripos($response,"errorMessage") === false) {
+			$jsonout = json_decode($response);
+		} else {
+			echo "[".$response."]";
+		}
+	}
+	curl_close($ch);
+	
+} else {
+	$errors['error'] = "Error: No car registration submitted";
+	echo json_encode($errors);
+}
+
+$output = serialize($jsonout);
+$dataout = unserialize($output);
+print_r($dataout);
+
+echo "Car Reg:".$jsonout[0]->registration."<hr />";
+echo "Last MOT: ".$jsonout[0]->motTests[0]->expiryDate."<hr />";
+echo "<h3>MOT History</h3>";
+foreach($jsonout[0]->motTests AS $mots) {
+	echo "<hr /><p>MOT Date: ".$mots->completedDate."<br />MOT Result: ".$mots->testResult."<br />Car Mileage:".$mots->odometerValue."</p>";
+}
+
+echo "<textarea style=\"width:100%; height:500px;\">";
+print_r($jsonout);
+echo "</textarea>";
+
+?>
--- addcarform.php
+++ addcarform.php
@@ -1,17 +1,69 @@
+<?php
+
+define("APIKEY","ptZAjbJWQO2LSzitce0JM9wZo4ASO2yV7XWGmoD7");
+define("ROOTURL","https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests");
+
+$myreg  = preg_replace('/[^a-zA-Z0-9]/','',$_POST['cr']);
+
+$errors = array();
+$jsonout = array();
+
+if (strlen($myreg)>2 AND $_POST['do'] == "getdvla") {
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,ROOTURL."?registration=".$myreg);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json","x-api-key:".APIKEY));
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	$response = curl_exec($ch);
+
+	if (curl_errno($ch))   {
+		$error['errors'] = 'ERROR -> ' . curl_errno($ch) . ': ' . curl_error($ch);
+	} else {
+		if (stripos($response,"errorMessage") === false) {
+			$jsonout = json_decode($response);
+			$iok = "ok";
+		} else {
+			$ierror = "<h5 class=\"error\">Invalid car registration</h5>";
+		}
+	}
+	curl_close($ch);
+	
+}
+?>
+
 <form action="dashboard.php" method="post">
-	<input type="hidden" value="addcar" name="do" />
+	<input type="hidden" name="do" value="<?=($_POST['do'] == "getdvla" AND $iok == "ok")?"addcar":"getdvla";?>" />
+	<input type="hidden" name="dataout" value="<?=serialize($jsonout);?>" />
 		<h3>Add a Car:</h3>
+		<?=$ierror;?>
 		<div class="form-group">
 			<label for="cr">Car Registration</label>
-			<input type="text" name='cr' class="form-control" id="cr" placeholder="Car Registration" required>
+			<input type="text" name='cr' class="form-control" id="cr" placeholder="Car Registration" value="<?=$jsonout[0]->registration;?>" required <?=($_POST['do'] == "getdvla" AND $iok == "ok")?" readonly":"";?>>
 		</div>
+		<?php if ($iok == "ok") { ?>
 		<div class="form-group">
-			<label for="rd">How many days before you want to be reminded?</label>
-			<select name="rd">
-				<option value="1week">1 Week</option>
-				<option value="2week">2 Weeks</option>
-				<option value="1month">1 Month</option>
+			<p class="lead">If the information below if correct, please add your reminder by selecting reminder timescale below.</p>
+			<table class="table table-sm">
+				<tr><th>Car make</th><td><?=$jsonout[0]->make;?></td></tr>
+				<tr><th>Car model</th><td><?=$jsonout[0]->model;?></td></tr>
+				<tr><th>Car colour</th><td><?=$jsonout[0]->primaryColour;?></td></tr>
+				<tr><th>Last MOT date</th><td><?=$jsonout[0]->motTests[0]->completedDate;?></td></tr>
+				<tr><th>MOT Status</th><td><?=$jsonout[0]->motTests[0]->testResult;?></td></tr>
+				<tr><th>Last MOT mileage</th><td><?=$jsonout[0]->motTests[0]->odometerValue;?></td></tr>
+				<tr><th>MOT due date</th><td><?=$jsonout[0]->motTests[0]->expiryDate;?></td></tr>
+			</table>
+		</div>
+		<?php } ?>		
+		<div class="form-group">
+			<label for="rd">How many days before do you want to be reminded?</label>
+			<select name="rd" id="rd">
+				<option value="1 week">1 Week</option>
+				<option value="2 weeks">2 Weeks</option>
+				<option value="1 month">1 Month</option>
 			</select>
 		</div>
-		<button type="submit" class="btn re-button">Add Car</button>
+		<button type="submit" class="btn re-button"><?=($_POST['do'] == "getdvla" AND $iok == "ok")?"Add Car":"Check Car Detail";?></button>
 </form>
