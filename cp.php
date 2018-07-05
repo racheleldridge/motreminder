e3ccb5306923bd1aa4c272e86b80dff09361e08e
racheleldridge <racheleldridge99@gmail.com>
2018-07-05 09:15:20 +0100

    Change password
    Instructions change colour when password is the correct format

--- cp.php
+++ cp.php
@@ -12,11 +12,4 @@
 			<input type="password" class="form-control" id="cpw" placeholder="Confirm Password" name="cpw" required>
 		</div>
 	<button type="submit" class="btn re-button" name="submit" value="submit">Reset Password</button>
-	<div id="message">
-		<h3>Password must contain the following:</h3>
-		<p id="letter" class="invalid">A <b>lowercase</b> letter</p>
-		<p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
-		<p id="number" class="invalid">A <b>number</b></p>
-		<p id="length" class="invalid">Minimum <b>8 characters</b></p>
-	</div>
 </form>
